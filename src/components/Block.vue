7946b352bf8fb7011aec0a1ce524520873657d56
unknown <lukasz.muraszka@gmail.com>
2022-07-19 16:18:30 +0200

    Add new files

---
+++ src/components/Block.vue
@@ -0,0 +1,59 @@
+<template>
+ <div class="item">
+  <div class="block" v-if="showBlock" @click="stopTimer">
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['delay'],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true
+      this.startTimer()
+    }, this.delay)
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10
+      }, 10)
+    },
+    stopTimer() {
+      clearInterval(this.timer)
+      this.$emit('end', this.reactionTime)
+    }
+  }
+
+}
+</script>
+
+<style>
+.block {
+  padding: 40px;
+  width: 5.1em;
+  height: 5.1em;
+  margin-left: 120px;
+  margin-top: 40px;
+  background-color: #ffe135;
+  border-radius: 50% 15%;
+  box-shadow: inset 0.4em 0.4em  #ccb42a;
+  transform: rotate(225deg);
+  position: absolute;
+
+}
+.item
+{
+  width:400px;
+  position: relative;
+  margin: auto;
+  }
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,35 @@
+<template>
+  <p>Reaction time - {{score}} ms</p>
+  <p class="rank">{{rank}}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+      return {
+        rank:null
+      }
+    },
+    mounted() {
+      if (this.score < 200) {
+        this.rank = 'Good reflexes!'
+      } else if (this.score < 400) {
+        this.rank = 'Average time, try again'
+      } else {
+        this.rank = 'You are weak, You have to practice...'
+      }
+      }
+    }
+
+
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #a4af0f;
+  font-weight: bold;
+}
+
+</style>
